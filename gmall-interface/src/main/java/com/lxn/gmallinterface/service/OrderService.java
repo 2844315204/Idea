b4a0364b15b58b5f2a9882680e7908dbe3446184
ÁõХÄÐ <2844315204@qq.com>
2020-06-29 16:13:35 +0800

    dubbo4

--- gmall-interface/src/main/java/com/lxn/gmallinterface/service/OrderService.java
+++ gmall-interface/src/main/java/com/lxn/gmallinterface/service/OrderService.java
@@ -5,5 +5,5 @@ import com.lxn.gmallinterface.bean.UserAddress;
 import java.util.List;
 
 public interface OrderService {
-    public List<UserAddress> initOrder(String userId);
+    public List<UserAddress> initOrder(String userIds);
 }
